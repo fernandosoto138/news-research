cc48623f3492d6d52df8d5099e49ece1750ebdce
Fernando Soto <fernando.soto.94@hotmail.com>
2018-02-18 18:01:37 -0300

    Moved postgre helpers

--- newsresearch/db_models/postgre_helpers.py
+++ newsresearch/db_models/postgre_helpers.py
@@ -3,5 +3,5 @@ from time import gmtime, strftime
 
 class PostgreHelpers(object):
     @staticmethod
-    def get_timestamp(self):
+    def get_timestamp():
         return strftime("%Y-%m-%d %H:%M:%S", gmtime())
