ab9a678a210c20f168e61a6ba0111a0dab011150
Fernando Soto <fernando.soto.94@hotmail.com>
2018-02-18 11:37:08 -0300

    Fixed attribute self

--- newsresearch/db_models/tables/postgre/postgre_helpers.py
+++ newsresearch/db_models/tables/postgre/postgre_helpers.py
@@ -3,5 +3,5 @@ from time import gmtime, strftime
 
 class PostgreHelpers(object):
     @staticmethod
-    def get_timestamp():
+    def get_timestamp(self):
         return strftime("%Y-%m-%d %H:%M:%S", gmtime())
